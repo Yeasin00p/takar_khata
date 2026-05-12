db76fdbd743a8ee9b8b6fe3be391f50688ca1e14
Yeasin00p <07yeasin1981@gmail.com>
2026-05-12 16:20:53 +0600

    raising add payment ui

--- lib/features/raising/presentation/screen/raising_screen.dart
+++ lib/features/raising/presentation/screen/raising_screen.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:takar_khata/core/theme/app_theme.dart';
+import 'package:takar_khata/features/raising/presentation/widget/show_add_sheet_widget.dart';
 
 class RaisingScreen extends StatelessWidget {
   const RaisingScreen({super.key});
 
+  void _openAddPaymentSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (_) => const AddPaymentSheet(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Raisings',
+          style: TextStyle(fontWeight: FontWeight.bold, letterSpacing: 1.2),
+        ),
+        backgroundColor: AppTheme.green,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(36),
+          child: Container(
+            color: AppTheme.green,
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Total Raised: ৳',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _openAddPaymentSheet(context),
+        backgroundColor: AppTheme.green,
+        icon: Icon(Icons.add, color: Colors.white),
+        label: const Text('Add Payment', style: TextStyle(color: Colors.white)),
+      ),
+    );
   }
 }
